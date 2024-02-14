3e3ee2d299582cf1f6b7688d388104d6e616fb4a
Deepa <deepahebballi4@gmail.com>
2024-02-14 15:26:43 +0530

    adding two files for extendedCalculator branch

---
+++ extendedCalculator.java
@@ -0,0 +1,15 @@
+public class extendedCalculator {
+
+    float triangle() {
+        float operand_1 = 10;
+        float operand_2 = 5;
+        return (1 / 2 * operand_1 * operand_2);
+    }
+
+    float modulus() {
+        float operand_1 = 10;
+        float operand_2 = 5;
+        return (operand_1 % operand_2);
+
+    }
+}
